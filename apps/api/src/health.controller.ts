71ec20d530ad83534d13583282056c9f7048ee7a
GOLLY DIMBO ERNEST ROMEO <gollydimbo@gmail.com>
2026-09-07 13:00:13 +0000

    Update health.controller.ts

--- apps/api/src/health.controller.ts
+++ apps/api/src/health.controller.ts
@@ -1,7 +1,8 @@
 import { Controller, Get } from '@nestjs/common';
 import { PrismaService } from './common/prisma.service';
+import { Public } from './common/public.decorator';
 @Controller('health')
 export class HealthController {
   constructor(private readonly db:PrismaService){}
-  @Get() async health(){ await this.db.$queryRaw`SELECT 1`; return {status:'ok',database:'up',timestamp:new Date().toISOString()}; }
+  @Public() @Get() async health(){ await this.db.$queryRaw`SELECT 1`; return {status:'ok',database:'up',timestamp:new Date().toISOString()}; }
 }
