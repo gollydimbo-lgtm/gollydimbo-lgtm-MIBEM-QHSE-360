cbc2331026932fd67b3c0216e518e35d7b7377d3
golly dimbo ernest romeo <gollydimbo@gmail.com>
2026-09-12 22:48:34 +0000

    Correction noms migrations et app.module.ts

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,4 +1,3 @@
-import { HygieneModule } from './hygiene/hygiene.module';
 import { Module } from '@nestjs/common';
 import { APP_GUARD } from '@nestjs/core';
 import { ConfigModule } from '@nestjs/config';
@@ -17,4 +16,4 @@ import { AttachmentsModule } from './attachments/attachments.module';
 import { DashboardModule } from './dashboard/dashboard.module';
 import { RecommendationsModule } from './recommendations/recommendations.module';
 import { HealthController } from './health.controller';
-@Module({imports:[ConfigModule.forRoot({isGlobal:true}),AuthModule,UsersModule,AuditModule,EpiModule,SyncModule,DocumentsModule,QhsModule,BusinessModule,QualityModule,AttachmentsModule,DashboardModule,RecommendationsModule,HygieneModule],controllers:[HealthController],providers:[PrismaService,{provide:APP_GUARD,useClass:JwtAuthGuard}]}) export class AppModule {}
+@Module({imports:[ConfigModule.forRoot({isGlobal:true}),AuthModule,UsersModule,AuditModule,EpiModule,SyncModule,DocumentsModule,QhsModule,BusinessModule,QualityModule,AttachmentsModule,DashboardModule,RecommendationsModule],controllers:[HealthController],providers:[PrismaService,{provide:APP_GUARD,useClass:JwtAuthGuard}]}) export class AppModule {}
